023dce2e143810d073d11cbb59c26eaa4eb2464c
Zeeshan Khaliq <shan@Zeeshans-Mac.local>
2013-08-22 17:50:22 -0700

    Initial Commit

---
+++ Lesson1/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Lesson1
+//
+//  Created by Zeeshan Khaliq on 8/22/13.
+//  Copyright Zeeshan Khaliq 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Lesson1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson1
+//
+//  Created by Zeeshan Khaliq on 8/22/13.
+//  Copyright Zeeshan Khaliq 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
